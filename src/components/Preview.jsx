4944cec682004d76b33e25f4f7f732d16b7c1fd7
s111ew <167631312+s111ew@users.noreply.github.com>
2025-01-30 13:36:36 +0000

    Format preview section

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,17 +1,81 @@
 function Preview() {
   return (
-    <section>
+    <section className="preview-section">
       <h2>Preview</h2>
       <div className="cv-preview">
-        <div>
-          <h3>Name</h3>
-          <p>Job Title</p>
-          <p>Website</p>
-          <p>Email Address</p>
-          <p>Phone Number</p>
-          <p>Location</p>
+          <div className="header">
+            <div className="name">
+              <h3>John</h3>
+              <h3>Smith</h3>
+              <p>Software Engineer</p>
+            </div>
+            <p>United States</p>
+          </div>
+          <div className="info">
+            <p>www.sam.tips</p>
+            <p>sam@gmail.com</p>
+            <p>+44 7942763247</p>
+          </div>
+          <div className="about-me">
+            <h4>About Me</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et elit porttitor, venenatis urna quis
+               dignissim quam. Quisque sagittis, nisi non elementum efficitur, le le pulvinar lacus, eleifend convallis dolor
+               turpis a mauris. Duis a efficitur elit, vitae accumsan mauris. Pellentesque habitant morbi tristique senectus et
+               netus et malesuada fames ac turpis egestas. Nunc ut iaculis nunc. Etiam porta, arcu accumsan tincidunt
+               ullamcorper, mi est aliquam leo, eu dictum odio elit laoreet mi. Aenean ac magna ex. Maecenas nec mi id
+               nisl mollis gravida. Proin pulvinar sodales massa.</p>
+          </div>
+          <div className="education-container">
+            <h4>Education</h4>
+            <div className="education entry">
+              <p><span>Harvard University</span><span>August 2024</span></p>
+              <p><span>BA Computer Science</span><span>United States</span></p>
+            </div>
+            <div className="education entry">
+              <p><span>High School</span><span>September 2020</span></p>
+              <p><span>4 x A-levels</span><span>United States</span></p>
+            </div>
+          </div>
+          <div className="experience-container">
+            <h4>Experience</h4>
+            <div className="experience entry">
+              <p><span>Google</span><span>September 2024 - Current</span></p>
+              <p><span>Junior Software Engineer</span><span>Silicon Valley, United States</span></p>
+              <ul>
+                <li>Quisque sagittis, nisi non elementum efficitur, leo leo pulvinar lacus, eleifend convallis dolor turpis a
+                mauris. Duis a efficitur elit, vitae accumsan mauris.</li>
+                <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc ut
+                iaculis nunc. Etiam porta, arcu accumsan tincidunt ullamcorper, mi est aliquam leo, eu dictum odio elit
+                aoreet mi.</li>
+              </ul>
+            </div>
+            <div className="experience entry">
+              <p><span>Meta</span><span>September 2023 - March 2023</span></p>
+              <p><span>Software Engineering Intern</span><span>Silicon Valley, United States</span></p>
+              <ul>
+                <li>Quisque sagittis, nisi non elementum efficitur, leo leo pulvinar lacus, eleifend convallis dolor turpis a
+                mauris. Duis a efficitur elit, vitae accumsan mauris.</li>
+                <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc ut
+                iaculis nunc. Etiam porta, arcu accumsan tincidunt ullamcorper, mi est aliquam leo, eu dictum odio elit
+                aoreet mi.</li>
+              </ul>
+            </div>
+          </div>
+          <div className="projects-container">
+            <h4>Projects</h4>
+            <div className="project entry">
+              <p><span>To-Do List</span><span>April 2024</span></p>
+              <p><span>JavaScript, TypeScript, React</span><span>United States</span></p>
+              <ul>
+                <li>Quisque sagittis, nisi non elementum efficitur, leo leo pulvinar lacus, eleifend convallis dolor turpis a
+                mauris. Duis a efficitur elit, vitae accumsan mauris.</li>
+                <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc ut
+                iaculis nunc. Etiam porta, arcu accumsan tincidunt ullamcorper, mi est aliquam leo, eu dictum odio elit
+                aoreet mi.</li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
     </section>
   )
 }
